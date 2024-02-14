e7178597cf50ec23af42344ce51629f997647286
Tin Htet <49719484+tinhtet5@users.noreply.github.com>
2024-02-14 13:40:20 +0630

    Update UserRepository.java

--- src/main/java/ai/group2/project_management_system/repository/UserRepository.java
+++ src/main/java/ai/group2/project_management_system/repository/UserRepository.java
@@ -11,12 +11,12 @@ import java.util.Set;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-<<<<<<< HEAD
+
 
     List<User> findByIdIn(List<Long> userIds);
 
     List<User> findByDepartmentId(Long departmentId);
-=======
+
     Optional<User> findByEmail(String email);
->>>>>>> ba01fb1f05e63036b65785dbb89732e82390d959
+
 }
