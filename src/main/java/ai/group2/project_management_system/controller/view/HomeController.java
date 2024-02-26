4a7d3944d3971708ac3dc77380a2a0a98a06a45f
tinhtet5 <tinhtet.tum@gmail.com>
2024-02-26 15:37:53 +0630

    two images erase

--- src/main/java/ai/group2/project_management_system/controller/view/HomeController.java
+++ src/main/java/ai/group2/project_management_system/controller/view/HomeController.java
@@ -1,13 +1,14 @@
 package ai.group2.project_management_system.controller.view;
 
-import ai.group2.project_management_system.model.Enum.Role;
+
 import ai.group2.project_management_system.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -15,13 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     private final UserService userService;
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model, HttpSession session){
         var user = userService.getCurrentUser();
 //        session.setAttribute("id",user.getId());
 //        session.setAttribute("role",user.getRole());
         /*
         System.out.println("UserId:"+user.getId());
         System.out.println("UserName:"+user.getName());*/
+        session.setAttribute("user", user);
         return "index";
     }
 
--- target/classes/static/assets/js/chat.js
+++ target/classes/static/assets/js/chat.js
@@ -15,23 +15,27 @@ document.addEventListener('DOMContentLoaded',()=>{
         const messages = document.getElementById('messages');
         const li = document.createElement('li');
 
-        //const sender = message.sender || "Unknown";  // Check for undefined
+        const sender = message.sender || "Unknown";  // Check for undefined
         const content = message.content || "No content";  // Check for undefined
 
-        //console.log(sender);
+        console.log(sender);
         console.log(content);
-        //${message.sender}
-        li.textContent = `${message.content}`;
+        li.textContent = `${message.sender} : ${message.content}`;
+        // const user = sessionStorage.getItem('user');
+        // const currentUser = JSON.parse(user);
+        if (sender === "Tin Htet") { // Replace "current_user" with the actual current user identifier
+            li.classList.add("own-message");
+        } else {
+            li.classList.add("other-message");
+        }
         messages.appendChild(li);
     }
-
-
-
 
 });
 
 function sendMessage() {
-    const sender = "Mg Mg";//document.getElementById('sender').value;
+
+    const sender = document.getElementById('sender').value;
     const content = document.getElementById('content').value;
     const message = {sender, content};
     stompClient.send('/app/chat', {}, JSON.stringify(message));
